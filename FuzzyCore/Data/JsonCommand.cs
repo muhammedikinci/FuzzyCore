74373f15de7812df2a4f5c0cff6275e0d15f6d2b
Muhammed123 <muhammedikinci@outlook.com>
2017-08-26 19:43:58 +0300

    Change Namespace

--- FuzzyCore/Data/JsonCommand.cs
+++ FuzzyCore/Data/JsonCommand.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net.Sockets;
 
-namespace fuzzyControl.Server.Data
+namespace FuzzyCore.Server.Data
 {
     public class JsonCommand
     {
