ee5b664211b909ac54c960efd163a221bbf3385b
Muhammed123 <muhammedikinci@outlook.com>
2017-08-26 19:43:59 +0300

    Database Connection -> MongoDB

---
+++ FuzzyCore/Database/User.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuzzyCore.Database.Models
+{
+    public class User
+    {
+        public ObjectId _id { get; set; }
+        public string name { get; set; }
+        public string password { get; set; }
+        public string premission { get; set; }
+    }
+}
---
+++ FuzzyCore/Database/dataBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson.IO;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver.Linq;
+using MongoDB.Driver;
+using MongoDB.Driver.GeoJsonObjectModel;
+using MongoDB.Bson;
+
+namespace FuzzyCore.Database
+{
+    public class dataBase
+    {
+        MongoClient monClient = new MongoClient("mongodb://localhost:27017");
+        public IMongoDatabase monData;
+        public dataBase() { monData = monClient.GetDatabase("NetworkApp"); }
+    }
+}
