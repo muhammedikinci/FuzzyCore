249550b5bf0e2c5644ab02be9f3dc8ba2a1c2380
Muhammed123 <muhammedikinci@outlook.com>
2017-08-26 19:43:58 +0300

    Change Namespace

--- FuzzyCore/Data/DataParser.cs
+++ FuzzyCore/Data/DataParser.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
-using fuzzyControl.Server.Data;
+using FuzzyCore.Server.Data;
 using System.Net.Sockets;
 using System.Net;
 using System.Diagnostics;
 using System.Threading;
 
-namespace fuzzyControl.Server
+namespace FuzzyCore.Server
 {
     class DataParser
     {
