da0159f5eb10ed22ebc23d9d2c0c2a4528296de5
Muhammed123 <muhammedikinci@outlook.com>
2017-08-26 19:43:58 +0300

    Change Namespace

--- FuzzyCore/ConsoleMessage.cs
+++ FuzzyCore/ConsoleMessage.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace fuzzyControl.Server
+namespace FuzzyCore.Server
 {
-    class ConsoleMessage
+    public class ConsoleMessage
     {
         public enum MessageType
         {
