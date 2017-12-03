36ecd9ed6975c2fa31b7a8380203ed7a35114101
Muhammed123 <muhammedikinci@outlook.com>
2017-12-03 12:07:00 +0300

    Add initialize type

--- FuzzyCore/Initialize/Init.cs
+++ FuzzyCore/Initialize/Init.cs
@@ -23,17 +23,20 @@ namespace FuzzyCore.Initialize
                 return Host;
             }
         }
-
-        public static String ProgramJsonPath;
         ConsoleMessage Message = new ConsoleMessage();
         FuzzyServer Server;
         ServiceHost Host;
-        public Init(string ProgramJsonPathArg,Action<Client> AcceptTask, Action<string, Client> ReceiveTask, string Ip = "127.0.0.1" , string Port = "111" , bool StartServer = true ,bool StartWCFService = true,bool StartReceive = true,bool StartAccept = true)
+        public Init(string ProgramJsonPathArg,Action<Client> AcceptTask, Action<string, Client> ReceiveTask,  InitType.Type initilizeType = InitType.Type.BASIC, string Ip = "127.0.0.1" , string Port = "111" , bool StartServer = true ,bool StartWCFService = true,bool StartReceive = true,bool StartAccept = true)
         {
+            InitType Type = new InitType();
+            Type.Paths = new FilePaths();
+            Type.ServerProp = new Props();
+
+            Type.initType = initilizeType;
             Message.Write("Advanced Initializing Started", ConsoleMessage.MessageType.BACKPROCESS);
             //Program.Json Exits
-            ProgramJsonPath = ProgramJsonPathArg;
-            if (File.Exists(ProgramJsonPath))
+            Type.Paths.ProgramJson = ProgramJsonPathArg;
+            if (File.Exists(Type.Paths.ProgramJson))
             {
                 Message.Write("Program.Json - Ready!",ConsoleMessage.MessageType.SUCCESS);
             }
@@ -55,23 +58,65 @@ namespace FuzzyCore.Initialize
             Server.ReceiverTask = ReceiveTask;
             if (StartServer)
             {
-                Server.startListen();
+                Server.Init(Type);
             }
         }
 
         //Basic Start Initializing
         public Init()
         {
-            Message.Write("Basic Initializing",ConsoleMessage.MessageType.BACKPROCESS);
-            ProgramJsonPath = "Program.json";
+            InitType Type = new InitType();
+            Type.Paths = new FilePaths();
+            Type.ServerProp = new Props();
+            //Information Message
+            Message.Write("Basic Initializing", ConsoleMessage.MessageType.BACKPROCESS);
+
+            //
+            //File Paths Init
+            //
+            Type.Paths.ProgramJson = "Program.json";
+            Type.Paths.MacPermissions = "Permissions/Mac.json";
+            Type.Paths.IpPermissions = "Permissions/Ip.json";
+
+            //
+            //WCF Service Init
+            //
             Host = new ServiceHost(typeof(Services.FuzzyService));
             Host.Open();
             if (Host.State == CommunicationState.Opened)
             {
                 Message.Write("WCF Service is Running", ConsoleMessage.MessageType.BACKPROCESS);
             }
+
+            //
+            //Fuzzy Server Init
+            //
             Server = new FuzzyServer(new IPEndPoint(IPAddress.Any,5959));
-            Server.startListen();
+            Server.Init(Type);
+        }
+
+        public Init(InitType Type)
+        {
+            staticInitData.Current_Init_Type = Type;
+            if (!string.IsNullOrEmpty(Type.FirstConsole_Message))
+            {
+                Console.WriteLine(Type.FirstConsole_Message);
+            }
+            if (Type.Wcf_Running)
+            {
+                Host = new ServiceHost(typeof(Services.FuzzyService));
+                Host.Open();
+                if (Host.State == CommunicationState.Opened)
+                {
+                    Message.Write("WCF Service is Running", ConsoleMessage.MessageType.BACKPROCESS);
+                }
+            }
+            if (Type.Server_Running)
+            {
+                Server = new FuzzyServer(new IPEndPoint(IPAddress.Parse(Type.ServerProp.IP), int.Parse(Type.ServerProp.Port)));
+                Server.Init(Type);
+                Console.WriteLine("Listening : " + Type.ServerProp.IP + ":" + Type.ServerProp.Port);
+            }
         }
     }
 }
