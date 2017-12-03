3374449544f31f3c4a599cb185048da0ba6a0c58
Muhammed123 <muhammedikinci@outlook.com>
2017-12-03 12:09:15 +0300

    Config And Basic Initializing

---
+++ FuzzyCore/Initialize/ServerProperties.cs
@@ -0,0 +1,35 @@
+﻿namespace FuzzyCore.Initialize
+{
+    public class InitType
+    {
+        public enum Type
+        {
+            BASIC,
+            CONFIG
+        }
+        public Type initType = Type.BASIC;
+        public Props ServerProp { get; set; }
+        public FilePaths Paths { get; set; }
+        public bool Wcf_Running { get; set; }
+        public bool Server_Running { get; set; }
+        public string FirstConsole_Message { get; set; }
+    }
+    public class Props
+    {
+        public enum ServerType
+        {
+            REMOTING,
+            GAMESERVER,
+            DATATRANFSER
+        }
+        public ServerType TYPE = ServerType.REMOTING;
+        public string IP { get; set; }
+        public string Port { get; set; }
+    }
+    public class FilePaths
+    {
+        public string ProgramJson;
+        public string MacPermissions;
+        public string IpPermissions;
+    }
+}
