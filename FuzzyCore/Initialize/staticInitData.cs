51e7e13ab3a46ae12f9a8fb0b22dc734223be358
Muhammed123 <muhammedikinci@outlook.com>
2017-12-03 12:09:52 +0300

    initialize type data

---
+++ FuzzyCore/Initialize/staticInitData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuzzyCore.Initialize
+{
+    public class staticInitData
+    {
+        public static InitType Current_Init_Type;
+    }
+}
