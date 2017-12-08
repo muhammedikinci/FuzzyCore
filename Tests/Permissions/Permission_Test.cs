6ea16b3e482e1bd0483ec656344ea3b12053849c
Muhammed123 <muhammedikinci@outlook.com>
2017-12-08 21:49:08 +0300

    Add permission unit test

---
+++ Tests/Permissions/Permission_Test.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FuzzyCore.Permissions;
+using System.IO;
+
+namespace FuzzyTest.Permissions
+{
+    [TestClass]
+    public class Permission_Test
+    {
+        IpPermission IP_Perm = new IpPermission();
+        MacPermission MAC_Perm = new MacPermission();
+
+        [TestMethod]
+        public void Test_IP_Perm()
+        {
+            IP_Perm.FilePath = "Ip.json";
+            Assert.IsTrue(IP_Perm.FileControl());
+            IP_Perm.TargetIP = "127.0.0.1";
+            Assert.IsTrue(IP_Perm.PermissionControl());
+            string mContent = File.ReadAllText(IP_Perm.FilePath);
+            Assert.AreEqual(IP_Perm.FileContent.Length,mContent.Length);
+        }
+
+        [TestMethod]
+        public void Test_MAC_Perm()
+        {
+            MAC_Perm.FilePath = "Mac.json";
+            Assert.IsTrue(MAC_Perm.FileControl());
+            MAC_Perm.MacObject = new MacPermission.PermissionMac() { MacAddress = "nope" };
+            Assert.IsFalse(MAC_Perm.PermissionControl());
+            string mContent = File.ReadAllText(MAC_Perm.FilePath);
+            Assert.AreEqual(MAC_Perm.FileContent.Length, mContent.Length);
+        }
+
+    }
+}
