1ed4a76d2a1392d80afe9a9c4d90e50ccc387df2
muhammedikinci <muhammedikinci@outlook.com>
2018-04-01 12:07:58 +0300

    Command for Logging Tool

---
+++ FuzzyCore/CommandClasses/GetLogs.cs
@@ -0,0 +1,20 @@
+﻿using FuzzyCore.Commands;
+using FuzzyCore.Data;
+using FuzzyCore.Employee;
+using Newtonsoft.Json;
+
+namespace FuzzyCore.CommandClasses
+{
+    public class GetLogs
+    {
+        public string GetAll_WithCommand()
+        {
+            Logger L = new Logger();
+            string logs = L.GetLogs();
+            JsonCommand Com = new JsonCommand();
+            Com.CommandType = "get_logs";
+            Com.Text = logs;
+            return JsonConvert.SerializeObject(Com);
+        }
+    }
+}
---
+++ FuzzyCore/ConcreteCommands/GetLogs_Command.cs
@@ -0,0 +1,20 @@
+﻿using FuzzyCore.Commands;
+using FuzzyCore.Data;
+using FuzzyCore.CommandClasses;
+
+namespace FuzzyCore.ConcreteCommands
+{
+    class GetLogs_Command : Command
+    {
+        public GetLogs_Command(JsonCommand comm) : base(comm)
+        {
+        }
+
+        public override void Execute()
+        {
+            GetLogs logs = new GetLogs();
+            string data = logs.GetAll_WithCommand();
+            data.SendDataString(Comm.Client_Socket);
+        }
+    }
+}
